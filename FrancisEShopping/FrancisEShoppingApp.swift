ed314703ae21534341f985beaba60d876b155a09
francisstar <francis@starpay.com.ph>
2022-06-19 23:20:28 +0800

    Initial Commit

---
+++ FrancisEShopping/FrancisEShoppingApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrancisEShoppingApp.swift
+//  FrancisEShopping
+//
+//  Created by Francis Adelante on 6/19/22.
+//
+
+import SwiftUI
+
+@main
+struct FrancisEShoppingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
